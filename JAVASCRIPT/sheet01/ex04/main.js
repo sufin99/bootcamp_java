1a17cdf201cd0e1e791d46252ec8b7bb4e973f7a
sufin99 <sufianzaghdad@gmail.com>
2024-06-18 12:15:44 +0200

    Subida ex04 sheet01

---
+++ JAVASCRIPT/sheet01/ex04/main.js
@@ -0,0 +1,24 @@
+let a = 0;
+let b = 33;
+let c = -42;
+
+if (a == 0)
+	console.log("El número", a, "es cero")
+else if (a > 0)
+	console.log("El número", a, "es positivo")
+else
+	console.log("El número", a, "es negativo")
+
+if (b == 0)
+	console.log("El número", b, "es cero")
+else if (b > 0)
+	console.log("El número", b, "es positivo")
+else
+	console.log("El número", b, "es negativo")
+
+if (c == 0)
+	console.log("El número", c, "es cero")
+else if (c > 0)
+	console.log("El número", c, "es positivo")
+else
+	console.log("El número", c, "es negativo")
