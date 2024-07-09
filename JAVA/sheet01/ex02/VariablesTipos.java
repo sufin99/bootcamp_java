e19e271aa563c812de58245239146ec7c0097099
sufin99 <sufianzaghdad@gmail.com>
2024-07-09 13:58:16 +0200

    subida ex02 sheet01

---
+++ JAVA/sheet01/ex02/VariablesTipos.java
@@ -0,0 +1,16 @@
+public class VariablesTipos {
+
+	public static void main(String[] args) {
+		int a = 5;
+		double b = 5.1;
+		boolean c = true;
+		char d = 'a';
+		String e = "Hola, Mundo";
+
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+	}
+}
