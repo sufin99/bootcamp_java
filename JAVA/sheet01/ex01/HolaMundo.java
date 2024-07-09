ceb92c30a3bb41606dbe021faedef8499633439a
sufin99 <sufianzaghdad@gmail.com>
2024-07-09 12:59:32 +0200

    subida ex01 sheet01

---
+++ JAVA/sheet01/ex01/HolaMundo.java
@@ -0,0 +1,9 @@
+/*ex01: Hola Mundo
+Escribe un programa que imprima "¡Hola, mundo!" en la consola. Llama a la
+clase HolaMundo .*/
+
+public class HolaMundo {
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+	}
+}
