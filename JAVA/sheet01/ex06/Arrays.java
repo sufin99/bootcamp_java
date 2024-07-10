2716bd54e5400924e710a6dafe66ea80fa9f0b73
sufin99 <sufianzaghdad@gmail.com>
2024-07-10 13:49:44 +0200

    Subida ex06 sheet01

---
+++ JAVA/sheet01/ex06/Arrays.java
@@ -0,0 +1,12 @@
+/*Ejercicio 6: Arrays
+Declara un array de enteros con 5 elementos, asígnales valores y luego
+imprime cada elemento del array. Llama a la clase Arrays .*/
+
+public class Arrays {
+	public static void main(String[] args) {
+		int crotolamo[] = {1, 5, 7, 9};
+		for (int i = 0; i < crotolamo.length; i++) {
+			System.out.println(crotolamo[i]);
+		}
+	}
+}
