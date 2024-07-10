194d80f27c9ca7e1d420036834f832d5abcebcb9
sufin99 <sufianzaghdad@gmail.com>
2024-07-10 13:49:10 +0200

    Subida ex05 sheet01

---
+++ JAVA/sheet01/ex05/BucleFor.java
@@ -0,0 +1,11 @@
+/*Ejercicio 5: Estructuras de Control
+Escribe un programa que imprima los números del 1 al 10 utilizando un bucle
+for . Llama a la clase BucleFor .*/
+
+public class BucleFor {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
+	}
+}
